fb1a0f54e40579fef2969fc907c520d680f70b01
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2026-03-12 16:39:51 -0400

    changed to set user info as variables and api key as environment variable

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 }
 
 provider "komodor" {
-  api_key = var.api_key
+  
 }
 
 resource "komodor_user" "my-user" {
-  display_name = "tf-user"
-  email = "tf-user@example.com"
+  display_name = var.user_name
+  email = var.user_email
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "api_key" {
-  description = "Komodor API Key"
-  default     = "da0da6e3-8ebc-4498-a152-2cc8a9129cae"
+variable "user_name" {
+  description = "user name"
+  default     = "tf-user"
+}
+
+variable "user_email" {
+  description = "user email"
+  default     = "tf-user@example.com"
 }
 

61f97f41a1da50c5a81ef41119dd7bc9d6818307
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2026-03-12 16:15:22 -0400

    modifications for Komodor provider

--- main.tf
+++ main.tf
@@ -1,31 +1,11 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "aws" {
-  region = var.region
+provider "komodor" {
+  api_key = var.api_key
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+resource "komodor_user" "my-user" {
+  display_name = "tf-user"
+  email = "tf-user@example.com"
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,8 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
-}
-
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
-}
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "api_key" {
+  description = "Komodor API Key"
+  default     = "da0da6e3-8ebc-4498-a152-2cc8a9129cae"
 }
 

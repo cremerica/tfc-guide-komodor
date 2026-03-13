c890abef27c88e0db6553550a0dbfb6832db1ae3
Fernando Cremer <fernandocremer@Fernandos-MacBook-Pro.local>
2026-03-13 12:54:01 -0400

    added second user resource

--- main.tf
+++ main.tf
@@ -18,3 +18,8 @@ resource "komodor_user" "my-user" {
   display_name = var.user_name
   email = var.user_email
 }
+
+resource "komodor_user" "second_user" {
+  display_name = "Joe Shmo"
+  email = "shmomeister@collio.edu"
+}
